6d57db737feb222e82f025a8d92c083d206de8bd
SergeyFM <github@svistunov.su>
2023-02-24 23:11:18 +0300

    Second branch editing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Hello world! edited from the second branch");
         System.out.println("------------");
         System.out.println("Added in the second branch");
     }
